cbf7eee85e4a84510da8b8883e52be6ffa655a45
DinhCuong <159988337+dinhcuong261291@users.noreply.github.com>
2026-06-20 22:47:11 +0700

    stOPN.sol

--- contracts/stOPN.sol
+++ contracts/stOPN.sol
@@ -1,1 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract stOPN is ERC20, Ownable {
+
+    address public stakingContract;
+
+    constructor()
+        ERC20(
+            "Staked OPN",
+            "stOPN"
+        )
+        Ownable(msg.sender)
+    {}
+
+    modifier onlyStaking() {
+
+        require(
+            msg.sender ==
+            stakingContract,
+            "Not staking contract"
+        );
+
+        _;
+    }
+
+    function setStakingContract(
+        address _staking
+    )
+        external
+        onlyOwner
+    {
+        stakingContract =
+            _staking;
+    }
+
+    function mint(
+        address to,
+        uint256 amount
+    )
+        external
+        onlyStaking
+    {
+        _mint(
+            to,
+            amount
+        );
+    }
+
+    function burn(
+        address from,
+        uint256 amount
+    )
+        external
+        onlyStaking
+    {
+        _burn(
+            from,
+            amount
+        );
+    }
+}
