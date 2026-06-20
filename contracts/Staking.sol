944e96b209b80232b63b9d0d60adb8d37991f4f2
DinhCuong <159988337+dinhcuong261291@users.noreply.github.com>
2026-06-20 23:00:35 +0700

    Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,1 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Staking is Ownable {
+
+    mapping(address => uint256)
+        public balances;
+
+    uint256 public totalStaked;
+
+    event Stake(
+        address indexed user,
+        uint256 amount
+    );
+
+    event Unstake(
+        address indexed user,
+        uint256 amount
+    );
+
+    constructor()
+        Ownable(msg.sender)
+    {}
+
+    function stake()
+        external
+        payable
+    {
+        require(
+            msg.value > 0,
+            "Zero amount"
+        );
+
+        balances[msg.sender]
+            += msg.value;
+
+        totalStaked
+            += msg.value;
+
+        emit Stake(
+            msg.sender,
+            msg.value
+        );
+    }
+
+    function unstake(
+        uint256 amount
+    )
+        external
+    {
+        require(
+            balances[msg.sender]
+            >= amount,
+            "Insufficient balance"
+        );
+
+        balances[msg.sender]
+            -= amount;
+
+        totalStaked
+            -= amount;
+
+        payable(msg.sender)
+            .transfer(amount);
+
+        emit Unstake(
+            msg.sender,
+            amount
+        );
+    }
+}
